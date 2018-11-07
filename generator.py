f01a67374c9f129632ebdcf9f8c8cc3225fb018c
Allison Bolen <allisonbolen@Allisons-MBP.attlocal.net>
2018-11-07 00:40:04 -0500

    cleaned

--- generator.py
+++ generator.py
@@ -7,46 +7,60 @@ import sys, os, shutil
 
 
 def main():
+    '''
+    This interprets the deadlock file as a graph and
+    saves each stage to an image file for loading into a gui
+    later on. It also tracks the text and saves them to a file.
+    '''
     setup(sys.argv[1])
+    # create an instance of the deadlock class
     dl = deadlock.deadlock(str(sys.argv[1]))
     steps = dl.getSteps()
     fig = plt.figure()
+    # initial set up of the graph
     names = dl.makeProcsLables() + dl.makeRescLables()
     processes = dl.getListProcs()
     resources = dl.getListResc()
     labels = createLabels(names, processes, resources)
-    count = 0
+
+    stepCount = 0
     step = "Initial"
 
-    # text bools
-    dead = False
-    stepText = ""
     edges = []
     while(step != "END GAME"):
-        prevEdges = edges
-
+        # get the new edges
         edges = convertStateToEdges(dl.getState(),resources, processes)
+        # are we in a state of deadlock based on the new edges
         dead, waitEdges = deadCheck(dl, processes)
-        graph = run(names, processes, resources, labels, edges, prevEdges)
-        stepText = textGen(step, count, steps, dl, stepText, dead, waitEdges)
-        # plt.text(5,5,stepText)
-        print(step)
-        print(dl.getState())
-        print()
+        # generate text description of the graph
+        stepText = textGen(step, stepCount, steps, dl, dead, waitEdges)
+        # create the graph given the new state of the game
+        graph = run(names, processes, resources, labels, edges)
         plt.axis('off')
+        # testing purpose
         # plt.pause(1)
-        #plt.draw()
-
-        saveFile(sys.argv[1], step, count, stepText)
+        # plt.draw()
+        # save the graph as and image and the text description for gui use
+        saveFile(sys.argv[1], step, stepCount, stepText)
+        # reset text and figure
         plt.clf()
         stepText = ""
-
+        # move to the next state of the game
         step = dl.nextStep()
-        count = count + 1
+        stepCount = stepCount + 1
 
     return
 
 def deadCheck(dl, processes):
+    '''
+    We only need to check for dead lock because ME, HW and NoP are coded into
+    the simulation so all we need to check for are cycles.
+    The best way to do this is to use a derivative of our state graph
+    called a wait for grph that shows what processes are dependent on other
+    processes. If we find a cycle in that directed graph then we are in
+    deadlock.
+    '''
+    # calculate the wait graph
     state = dl.getState()
     waitEdgeList = []
     for process in state:
@@ -54,60 +68,70 @@ def deadCheck(dl, processes):
             for edge in state[process]["waitingFor"]:
                 waitEdgeList.append((process,edge))
 
+    # make the graph
     graph = nx.DiGraph()
     graph.add_nodes_from(processes)
     graph.add_edges_from(waitEdgeList)
-    # nx.draw(graph, with_labels=True)
 
     try:
+        # cycle is found with built in function
         nx.find_cycle(graph, orientation='original')
         return True, waitEdgeList
     except:
         pass
     return False, waitEdgeList
 
-def textGen( step, count, steps, dl, stepText, dead, waitEdges):
+def textGen(step, count, steps, dl, dead, waitEdges):
+    '''
+    Generates text description for each state of the game.
+    '''
     text = ""
     if step == "Initial" :
         text = "This is a graph with "+str(dl.getNumProcs())+" processes and "+str(dl.getNumResc())+"resources."\
-        "Using this graph you will see the methodology of resource allocation."\
-        "Make sure to keep and eye out for deadlock conditions. Our system holds for mutual exclusion: no two processes can own the smae resource at the same time"\
-        ", hold and wait: a process must be holding at least one resouce and be waiting to aquire resources that are owned by another process, no preemption: process can not steal resources from "\
-        "another process before the process is finished with the resource, and circular: where there is a cycle of a process wating on a reousrce where the owner of that resource is waiting for the process who is wating for it."
-        "To see states click next."
+        " This is a graphical simulation of resource allocation."\
+        " Our system holds for mutual exclusion: no two processes can own the same resource at the same time,"\
+        " hold and wait: a process must be holding at least one resouce and be waiting to aquire resources that are owned by another process, no preemption: process can not steal resources from "\
+        " another process before a process is finished with a resource, and circular: there is a cycle of a processes dependent on other processes."
+        " Use the left and right arrows to navigate between steps of the simulation."
     else:
 
         if dead is True and len(steps) > 0:
-            # we are deadlocked and its not the end of teh system yet
-            text = "This is step "+str(count)+": '"+step+"'."\
-            "\nIn this step we see that we are in a dead lock state along these directed edges: "+str(waitEdges)+"."
+            # we are deadlocked and its not the end of the system yet
+            text = "Step "+str(count)+": '"+step+"'."\
+            " In this step we see that we are in a dead lock state along these directed edges: "+str(waitEdges)+"."
 
         elif dead is True and len(steps) == 0:
-            # we are in a state of deadlock and it is the last steps
-            text = "This is step "+str(count)+": '"+step+"'."\
-            "\nIn this step we see that we are in a dead lock state along these directed edges: "+str(waitEdges)+"."\
-            "\nThis is the last step and we are ending in a deadlocked state."
+            # we are in a state of deadlock and it is the last step
+            text = "Step "+str(count)+": '"+step+"'."\
+            " In this step we see that we are in a dead lock state along these directed edges: "+str(waitEdges)+"."\
+            " Where (x,y) x -> y. This is the last step and we are ending in a deadlocked state."
         elif dead is False and len(steps) > 0:
-            # we are not deadlocked but we are at the end of teh GAME
-            text = "This is step "+str(count)+": '"+step+"'."\
+            # we are not deadlocked but we not at the end of the GAME
+            text = " Step "+str(count)+": '"+step+"'."\
 
         elif dead is False and len(steps) == 0:
-            # we are not dead and we are not the END
-            text = "This is step "+str(count)+": '"+step+"'."\
-            "\nThis is the last step."
+            # we are not dead and we END
+            text = "Step "+str(count)+": '"+step+"'."\
+            " This is the last step."
 
     return text
 
 def setup(asocFile):
+    '''
+    Make/clean directories that we'll need to store images in
+
+    '''
     dir = asocFile[:asocFile.index(".")]
     if os.path.isdir(dir) is True:
         shutil.rmtree(dir)
-    if os.path.isdir(dir) is False:
-        os.mkdir(dir)
-        os.makedirs(dir+"/text")
-        os.makedirs(dir+"/images")
+    os.mkdir(dir)
+    os.makedirs(dir+"/text")
+    os.makedirs(dir+"/images")
 
 def saveFile(asocFile, step, count, text):
+    '''
+    Save the graph as and image and save the text for use in the Gui
+    '''
     # save figure
     dirname = os.path.dirname("deadlock.py")
     dir = asocFile[:asocFile.index(".")]
@@ -119,16 +143,19 @@ def saveFile(asocFile, step, count, text):
         print(text, file=text_file)
 
 def createLabels(names, processes, resources):
+    '''
+    Make the node labels
+    '''
     info = {}
     for item in range(0, (resources[len(resources)-1])+1):
         info[item]= names[item]
     return info
 
-def run(names, processes, resources, labels, edges, prevEdges):
+def run(names, processes, resources, labels, edges):
+    '''
+    Make the user facing graph based on our current edge state
 
-    print("OLDEDGES"+str(prevEdges))
-    print("NOWEDGES:"+str(edges))
-
+    '''
 
     B = nx.DiGraph()
     B.add_nodes_from(processes+resources)
@@ -137,6 +164,7 @@ def run(names, processes, resources, labels, edges, prevEdges):
     countP = 1
     countR = 1
 
+    # setup node location on the graph
     for node in processes+resources:
         if node in processes:
             B.node[node]['pos'] = (countP*10,10)
@@ -147,22 +175,14 @@ def run(names, processes, resources, labels, edges, prevEdges):
             pos[node]=(countR*10,80)
             countR = countR + 1
 
-    # for oldEdge in prevEdges:
-    #     found = False
-    #     for edge in edges:
-    #         if found is False and edge == oldEdge:
-    #             found = True
-    #     if found is False:
-    #         # we did not see the old edge in the new edges so we need to remove it from the DiGraph
-    #         B.remove_edge(oldEdge)
-
+    # graph the process nodes
     nx.draw_networkx_nodes(B, pos,
                                nodelist=processes,
                                node_color='g',
                                node_size=600,
                                alpha=0.8,
                                label="Processes")
-
+    # graph the resource nodes
     nx.draw_networkx_nodes(B, pos,
                                nodelist=resources,
                                node_color='y',
@@ -170,12 +190,12 @@ def run(names, processes, resources, labels, edges, prevEdges):
                                node_shape='s',
                                alpha=0.8,
                                label="Resources")
-
+    # graph the edges
     nx.draw_networkx_edges(B, pos,
                                edgelist=edges,
                                width=2, alpha=1, arrows=True, arrowstyle='->', arrowsize=20)
 
-
+    # draw everything properly
     nx.draw_networkx_labels(B, pos, labels, font_size=16)
 
 
@@ -196,11 +216,11 @@ def convertStateToEdges(state, resources, processes):
                     edgeList.append((resource, process))
 
         if len(state[process]["requested"]) > -1:
+            # we woant something do it should point the other way now
             for resource in state[process]["requested"]:
                 if (resource, process) not in edgeList:
                     edgeList.append((process, resource))
 
-    print(edgeList)
     return edgeList
 
 if __name__ == "__main__": main()
